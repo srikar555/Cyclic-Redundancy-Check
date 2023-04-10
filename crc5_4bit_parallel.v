37c2104271841e23adacf773ef6933e5bc84a87c
srikar555 <64193247+srikar555@users.noreply.github.com>
2023-04-10 12:56:16 +0530

    5 bit crc

---
+++ crc5_4bit_parallel.v
@@ -0,0 +1,27 @@
+module crc(
+  input [7:0] data_in,
+  input crc_en,
+  output [3:0] crc_out,
+  input rst,
+  input clk);
+
+  reg [3:0] lfsr_q,lfsr_c; // lfsr_q is Min, lfsr_c is Mout and data_in is Nin
+  assign crc_out = lfsr_q;
+
+  always @(*) begin
+    lfsr_c[0] = lfsr_q[3] ^ data_in[0] ^ data_in[1] ^ data_in[3] ^ data_in[7];
+    lfsr_c[1] = lfsr_q[0] ^ data_in[1] ^ data_in[2] ^ data_in[4];
+    lfsr_c[2] = lfsr_q[1] ^ lfsr_q[3] ^ data_in[0] ^ data_in[1] ^ data_in[2] ^ data_in[5] ^ data_in[7];
+    lfsr_c[3] = lfsr_q[2] ^ lfsr_q[3] ^ data_in[0] ^ data_in[2] ^ data_in[6] ^ data_in[7];
+
+  end // always
+
+  always @(posedge clk, posedge rst) begin
+    if(rst) begin
+      lfsr_q <= 4'b1111;
+    end
+    else begin
+      lfsr_q <= crc_en ? lfsr_c : lfsr_q;
+    end
+  end // always
+endmodule // crc
---
+++ crc5_4bit_parallel_tb.v
@@ -0,0 +1,26 @@
+`timescale  1ns/1ps
+module crc_tb;
+reg [7:0] data_in;
+reg crc_en,rst,clk;
+wire [3:0]crc_out;
+
+crc DUT(.data_in(data_in), .crc_en(crc_en), .crc_out(crc_out), .rst(rst), .clk(clk));
+initial
+begin
+crc_en = 1;
+rst = 0;
+clk = 0;
+
+#5 rst = ~rst;
+#5 rst = ~rst;
+data_in=8'hba;
+repeat(4)
+#5 clk = ~clk;
+
+#5 rst = ~rst;
+#5 rst = ~rst;
+data_in=8'hca;
+repeat(4)
+#5 clk = ~clk;
+end
+endmodule
